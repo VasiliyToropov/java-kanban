8e4a401915d759e1851c06b64921d05bb6b76498
Vasiliy <toropovforwork@mail.ru>
2024-05-28 23:38:40 +0300

    Решение задачи 8 спринта, исправление после теста (1)

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 import taskmanagers.FileBackedTaskManager;
 import taskmanagers.TaskManager;
-import tasks.*;
+import tasks.EpicTask;
+import tasks.SubTask;
+import tasks.Task;
+import tasks.TaskStatus;
 import utility.Managers;
+
 import java.io.File;
 import java.io.IOException;
 import java.time.LocalDateTime;
@@ -29,22 +33,22 @@ public class Main {
     private static void createTasks(FileBackedTaskManager manager) {
         // Создаем даты для задач
 
-        LocalDateTime startForTask1 = LocalDateTime.of(2024,5,5,12,0);
+        LocalDateTime startForTask1 = LocalDateTime.of(2024, 5, 5, 12, 0);
         long durationForTask1 = 30;
 
-        LocalDateTime startForTask2 = LocalDateTime.of(2024,5,5,12,15);
+        LocalDateTime startForTask2 = LocalDateTime.of(2024, 5, 5, 12, 15);
         long durationForTask2 = 30;
 
-        LocalDateTime startForEpic = LocalDateTime.of(2024,5,3,14,0);
+        LocalDateTime startForEpic = LocalDateTime.of(2024, 5, 3, 14, 0);
         long durationForEpic = 60;
 
-        LocalDateTime startForSubTask1 = LocalDateTime.of(2024,5,1,15,0);
+        LocalDateTime startForSubTask1 = LocalDateTime.of(2024, 5, 1, 15, 0);
         long durationForSubTask1 = 10;
 
-        LocalDateTime startForSubTask2 = LocalDateTime.of(2024,5,1,16,30);
+        LocalDateTime startForSubTask2 = LocalDateTime.of(2024, 5, 1, 16, 30);
         long durationForSubTask2 = 20;
 
-        LocalDateTime startForSubTask3 = LocalDateTime.of(2024,5,1,16,50);
+        LocalDateTime startForSubTask3 = LocalDateTime.of(2024, 5, 1, 16, 50);
         long durationForSubTask3 = 60;
 
         manager.createTask(new Task("Обычная задача - 1", "Описание обычной задачи",
@@ -60,7 +64,7 @@ public class Main {
                 manager.getId(), TaskStatus.NEW, 1, startForSubTask1, durationForSubTask1)); // id = 3
 
         manager.createSubTask(new SubTask("Подзадача - 2", "Описание подзадачи",
-                manager.getId(), TaskStatus.NEW, 1, startForSubTask2,  durationForSubTask2)); // id = 4
+                manager.getId(), TaskStatus.NEW, 1, startForSubTask2, durationForSubTask2)); // id = 4
 
         manager.createSubTask(new SubTask("Подзадача - 3", "Описание подзадачи",
                 manager.getId(), TaskStatus.NEW, 1, startForSubTask3, durationForSubTask3)); // id = 5
@@ -69,21 +73,21 @@ public class Main {
 
     private static void doSomeOperations(FileBackedTaskManager manager) {
 
-        for(Task task : manager.getAllTasks()) {
+        for (Task task : manager.getAllTasks()) {
             System.out.print(task.getName() + " ");
             System.out.print("Время начала: " + task.getStartTime() + " ");
             System.out.print("Продолжительность: " + task.getDuration() + " ");
             System.out.println("Дата окончания: " + manager.getEndTime(task));
         }
 
-        for(Task task : manager.getAllEpicTasks()) {
+        for (Task task : manager.getAllEpicTasks()) {
             System.out.print(task.getName() + " ");
             System.out.print("Время начала: " + task.getStartTime() + " ");
             System.out.print("Продолжительность: " + task.getDuration() + " ");
             System.out.println("Дата окончания: " + manager.getEndTime(task));
         }
 
-        for(Task task : manager.getAllSubtasks()) {
+        for (Task task : manager.getAllSubtasks()) {
             System.out.print(task.getName() + " ");
             System.out.print("Время начала: " + task.getStartTime() + " ");
             System.out.print("Продолжительность: " + task.getDuration() + " ");
@@ -91,7 +95,7 @@ public class Main {
         }
 
         //Печатаем еще раз эпик, чтобы убедиться, что время поменялось
-        for(Task task : manager.getAllEpicTasks()) {
+        for (Task task : manager.getAllEpicTasks()) {
             System.out.print(task.getName() + " ");
             System.out.print("Время начала: " + task.getStartTime() + " ");
             System.out.print("Продолжительность: " + task.getDuration() + " ");
